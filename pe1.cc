03e405ed88ad061a49e6943839bd8c1893aabf02
gloriankosi <kosig@acm.org>
2021-05-19 22:10:02 -0400

    c

---
+++ pe1.cc
@@ -0,0 +1,19 @@
+/**
+ * @file pe1.cc
+ * @author Glorian Kosi
+ * @brief Project Euler #1: Multiples of 3 and 5
+ */
+
+#include <iostream>
+using std::cin;
+using std::cout;
+int main()
+{
+    int t;
+    cin >> t;
+    for (int a0 = 0; a0 < t; a0++)
+    {
+        int n;
+        cin >> n;
+    }
+}
